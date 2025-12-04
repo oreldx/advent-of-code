fef096ac6acc8ce21ffdef74141c77baee41eb04
Orel <orel.dev@proton.me>
2025-12-04 14:08:31 +0100

    feat: ✨ Add problems 1 & 2 - day 3 (2025) (#15)
    * feat: :sparkles: Add p1
    
    * feat: :construction: Add p2
    
    * feat: :sparkles: Add p2

---
+++ year_2025/day_03/main.py
@@ -0,0 +1,77 @@
+DEBUG = False
+
+
+def open_input() -> list[list[int]]:
+    def line_parser(bank: str) -> list:
+        return [int(c) for c in bank]
+
+    if DEBUG:
+        print("DEBUG MODE ON")
+        sample = [
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ]
+        return [line_parser(bank) for bank in sample]
+
+    filepath = "input.txt"
+    banks = []
+    with open(filepath, "r", encoding="utf-8") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                return banks
+
+            line = line.strip()
+            banks.append(line_parser(line))
+
+
+def problem_1() -> int:
+    banks = open_input()
+    joltageSum = 0
+    for bank in banks:
+        maximum_ten = max(bank[:-1])
+        maximum_unit = max(bank[bank.index(maximum_ten) + 1 :])
+        joltageSum += int(f"{maximum_ten}{maximum_unit}")
+
+    return joltageSum
+
+
+def problem_2() -> int:
+    banks = open_input()
+
+    joltageSum = 0
+    required_length = 12
+    for bank in banks:
+        max_joltage = ""
+        last_idx = 0
+        for i in range(required_length, 0, -1):
+            available_range = len(bank) - i
+            available_batteries = bank[
+                last_idx : max(available_range + 1, last_idx + 1)
+            ]
+            maximum = max(available_batteries)
+            last_idx = available_batteries.index(maximum) + 1 + last_idx
+            max_joltage += str(maximum)
+
+        joltageSum += int(max_joltage)
+
+    return joltageSum
+
+
+def main() -> None:
+    if DEBUG:
+        print(problem_2())
+        return
+
+    match input("Choose which problem to print (1 or 2): "):
+        case "1":
+            print(problem_1())
+        case "2":
+            print(problem_2())
+        case _:
+            print("Invalid choice. Please enter 1 or 2.")
+
+
+main()
