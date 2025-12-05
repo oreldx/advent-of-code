6ef5d8668b6060c9a8ad3f011b787a0155b3ba9b
oreldx <orel.dev@proton.me>
2025-12-05 13:24:45 +0100

    feat: :sparkles: Add p2

--- year_2025/day_05/main.py
+++ year_2025/day_05/main.py
@@ -47,51 +47,61 @@ def open_input() -> list[list[int]]:
             data["ranges"].append(parsed_range)
 
 
-# def optimize_ranges(prev_ranges: list) -> list:
-#     new_ranges = []
-#     for prev_range in prev_ranges:
-#         # update_new_ranges(new_ranges, pr)
-#         hasOverlapped = False
-#         for new_range in new_ranges:
-#             if prev_range[0] > new_range[1] or prev_range[1] < new_range[0]:
-#                 # no overlapping at all
-#                 continue
-#             if prev_range[0] < new_range[0] and prev_range[1] > new_range[0]:
-#                 new_range[0] = prev_range[0]
-#             if prev_range[1] > new_range[1] and prev_range[0] < new_range[1]:
-#                 new_range[1] = prev_range[1]
-#             hasOverlapped = True
-#         if not hasOverlapped:
-#             new_ranges.append(prev_range)
-
-#     # After first iteration, it's possible some new ranges overlape together
-
-#     return new_ranges
-
-
 def problem_1() -> int:
     data = open_input()
 
-    # ranges = optimize_ranges(data["ranges"])
-    sum = 0
+    fresh_ids_count = 0
     for n in data["ids"]:
         for r in data["ranges"]:
             if n >= r[0] and n <= r[1]:
-                sum += 1
+                fresh_ids_count += 1
                 break
 
-    return sum
+    return fresh_ids_count
+
+
+def optimize_ranges(prev_ranges: list) -> list:
+    new_ranges = []
+    for prev_range in prev_ranges:
+        # update_new_ranges(new_ranges, pr)
+        hasOverlapped = False
+        for new_range in new_ranges:
+            if prev_range[0] > new_range[1] or prev_range[1] < new_range[0]:
+                # no overlapping at all
+                continue
+            if prev_range[0] < new_range[0] and prev_range[1] >= new_range[0]:
+                # prev range start earlier
+                new_range[0] = prev_range[0]
+            if prev_range[1] > new_range[1] and prev_range[0] <= new_range[1]:
+                # prev range end later
+                new_range[1] = prev_range[1]
+            hasOverlapped = True
+        if not hasOverlapped:
+            new_ranges.append(prev_range)
+
+    return new_ranges
 
 
 def problem_2() -> int:
     data = open_input()
 
-    return 0
+    original_ranges = data["ranges"]
+    opti_ranges = original_ranges
+    while True:
+        opti_ranges = optimize_ranges(original_ranges)
+        if len(opti_ranges) == len(original_ranges):
+            break
+        original_ranges = opti_ranges
+    possibles = 0
+    for r in opti_ranges:
+        possibles += r[1] - r[0] + 1
+
+    return possibles
 
 
 def main() -> None:
     if DEBUG:
-        print(problem_1())
+        print(problem_2())
         return
 
     match input("Choose which problem to print (1 or 2): "):
